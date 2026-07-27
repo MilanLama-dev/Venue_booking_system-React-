3a4ce310ae6121d6118f1bbf4f37439e5a6ea1e9
Milan <lamamilan3889@gmail.com>
2026-07-27 14:17:10 +0545

    Implement simple venue details page

--- src/pages/VenueDetails.jsx
+++ src/pages/VenueDetails.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import { venues } from '../data/venues'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const VenueDetails = () => {
+  const {id} = useParams()
+  const navigate = useNavigate()
+
+  const venue = venues.find((v) => v.id == parseInt(id))
+
+  if(!venue){
+    return(
+      <>
+      <Navbar />
+      <div className='max-w-4xl mx-auto py-10 px-6'>
+        <h1 className='text-3xl font-bold'>Venue not found</h1>
+      </div>
+      </>
+    )
+  }
+
+  const handleBooking = () => {
+    navigate("/confirm")
+  }
+
   return (
-    <div>VenueDetails</div>
+    <>
+    <Navbar />
+    <div className='max-w-4xl mx-auto py-10 px-6'>
+      <img src={venue.image} alt={venue.name} className='w-full h-96 '/>
+      <h1>{venue.name}</h1>
+      <div>
+        <p><strong>Location:</strong>{venue.location}</p>
+        <p><strong>Capacity:</strong>{venue.capacity}</p>
+        <p><strong>Price:</strong>{venue.price}</p>
+      </div>
+      <p></p>
+      <button onClick={handleBooking}>Book Now</button>
+    </div>
+    </>
   )
 }
 
